89e880a448a8b951ecfce55ecd85f7503cf050e1
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-02 13:48:44 -0400

    Edited a code

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-'use strict';
+"use strict";
 
 {
-  const btn = document.getElementById('btn');
-  const result = document.getElementById('result');
+  const btn = document.getElementById("btn");
+  const result = document.getElementById("result");
 
-  btn.addEventListener('click', () => {
-    const results = ['Super Lucky!', 'Lucky!', 'Ok lol'];
-    const n = Math.floor(Math.random() * results
-    .length);
+  btn.addEventListener("click", () => {
+    const results = [
+      "great blessing!!!",
+      "middle blessing!!",
+      "small blessing!",
+      "blessing",
+      "Uncertain luck",
+      "curse",
+    ];
+    const n = Math.floor(Math.random() * results.length);
 
-    result.textContent = results
-    [n];
-    
+    result.textContent = results[n];
+
     // switch (n) {
     //   case 0:
     //   result.textContent = 'Super Lucky!';
@@ -23,6 +28,5 @@
     //   result.textContent = 'Ok! lol';
     //   break;
     // };
-
   });
 }
