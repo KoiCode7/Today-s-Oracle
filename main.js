6b11349aa9a15b748a539788b55499a5a72cc825
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-02 14:03:35 -0400

    Edited a code to change possibility of result

--- main.js
+++ main.js
@@ -5,17 +5,33 @@
   const result = document.getElementById("result");
 
   btn.addEventListener("click", () => {
-    const results = [
-      "great blessing!!!",
-      "middle blessing!!",
-      "small blessing!",
-      "blessing",
-      "Uncertain luck",
-      "curse",
-    ];
-    const n = Math.floor(Math.random() * results.length);
+    const n = Math.random();
+    if (n < 0.15) {
+      result.textContent = "Great blessing!!!"; // 15%
+    } else if (n < 0.35) {
+      result.textContent = "Middle blessing!!"; // 20%
+    } else if (n < 0.65) {
+      result.textContent = "Small blessing!"; // 30%
+    } else if (n < 0.95) {
+      result.textContent = "Blessing"; // 30%
+    } else {
+      result.textContent = "Curse"; // 5%
+    }
 
-    result.textContent = results[n];
+    console.log(n);
+
+    // btn.addEventListener("click", () => {
+    //   const results = [
+    //     "great blessing!!!",
+    //     "middle blessing!!",
+    //     "small blessing!",
+    //     "blessing",
+    //     "Uncertain luck",
+    //     "curse",
+    //   ];
+    //   const n = Math.floor(Math.random() * results.length);
+
+    //   result.textContent = results[n];
 
     // switch (n) {
     //   case 0:
